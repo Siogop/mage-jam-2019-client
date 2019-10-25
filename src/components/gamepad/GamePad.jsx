e7554fd686fc9c6c011fac0052e2439ddae35005
Miłosz Pogodski <mpo@bcsg.pl>
2019-10-26 00:46:03 +0200

    Simplified messages

--- src/components/gamepad/GamePad.jsx
+++ src/components/gamepad/GamePad.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
 import './GamePad.scss';
 
-const MOVE_LEFT_COMMUNICAT = '{"messageType":"methodInvocation","data":"{\\"methodName\\":\\"Move\\",\\"arguments\\":[\\"left\\"]}"}';
-const MOVE_RIGHT_COMMUNICAT = '{"messageType":"methodInvocation","data":"{\\"methodName\\":\\"Move\\",\\"arguments\\":[\\"right\\"]}"}';
+const MOVE_LEFT_MESSAGE = '{"data":"{\\"methodName\\":\\"Move\\",\\"arguments\\":[\\"left\\"]}"}';
+const MOVE_RIGHT_MESSAGE = '{"data":"{\\"methodName\\":\\"Move\\",\\"arguments\\":[\\"right\\"]}"}';
+const ATTACK_MESSAGE = '{"data":"{\\"methodName\\":\\"Attack\\",\\"arguments\\":[\\"right\\"]}"}';
 
 class GamePad extends React.Component {
   constructor(props) {
@@ -16,32 +18,34 @@ class GamePad extends React.Component {
 
   onLeftClick() {
     const { webSocket } = this.props;
-    console.log(MOVE_LEFT_COMMUNICAT);
-    webSocket.send(MOVE_LEFT_COMMUNICAT); // send data to the server
-  }
-
-  onRightClick() {
-    const { webSocket } = this.props;
-    webSocket.send('attack'); // send data to the server
+    console.log(MOVE_LEFT_MESSAGE);
+    webSocket.send(MOVE_LEFT_MESSAGE); // send data to the server
   }
 
   onAttackCLick() {
     const { webSocket } = this.props;
-    console.log(MOVE_RIGHT_COMMUNICAT);
-    webSocket.send(MOVE_RIGHT_COMMUNICAT); // send data to the server
+    console.log(ATTACK_MESSAGE);
+    webSocket.send(ATTACK_MESSAGE); // send data to the server
+  }
+
+  onRightClick() {
+    const { webSocket } = this.props;
+    console.log(MOVE_RIGHT_MESSAGE);
+    webSocket.send(MOVE_RIGHT_MESSAGE); // send data to the server
   }
 
   render() {
     return (
       <div className="game-pad">
+        <div className="" />
         <div className="button-container">
-          <div onClick={this.onLeftClick}>Left</div>
+          <div role="button" tabIndex={0} onClick={this.onLeftClick}>Left</div>
         </div>
         <div className="button-container">
-          <div onClick={this.onAttackCLick}>Attack</div>
+          <div role="button" tabIndex={0} onClick={this.onAttackCLick}>Attack</div>
         </div>
         <div className="button-container">
-          <div onClick={this.onRightClick}>Right</div>
+          <div role="button" tabIndex={0} onClick={this.onRightClick}>Right</div>
         </div>
       </div>
     );
