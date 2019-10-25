29be2fa6983b0307431e450e5679fa1f5f57f54e
Piotr Ptak <poe@the-raven.local>
2019-10-26 01:56:12 +0200

    Create App Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Main from './components/main/Main';
 import './styles/main.scss';
 import '../node_modules/nes.css/css/nes.min.css';
 import AppHeader from './components/AppHeader/AppHeader';
+import AppFooter from './components/AppFooter/AppFooter';
 
 function App() {
   return (
     <div className="layout">
       <AppHeader />
       <Main />
+      <AppFooter />
     </div>
   );
 }
---
+++ src/components/AppFooter/AppFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AppFooter.scss';
+
+export default function AppFooter() {
+  return (
+    <header className="app-footer">
+      <a href="https://github.com/lukaszkobylecki/MageJam2019">
+        <i className="nes-icon github is-large" />
+      </a>
+    </header>
+  );
+}
