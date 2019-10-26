3fbc2ca4ea43635577fbed55d58184550e3faeaa
Poe <piotr@ptak.dev>
2019-10-26 15:24:05 +0200

    Add dynamic app header

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './AppHeader.scss';
+import PropTypes from 'prop-types';
 
-export default function AppHeader() {
+export default function AppHeader({ title }) {
   return (
     <header className="app-header">
-      <div className="nes-container is-rounded is-dark">Mage Jam 2019</div>
+      <div className="nes-container is-rounded is-dark">{title}</div>
     </header>
   );
 }
+
+AppHeader.propTypes = {
+  title: PropTypes.string,
+};
+
+AppHeader.defaultProps = {
+  title: 'Mage Jam 2019',
+};
