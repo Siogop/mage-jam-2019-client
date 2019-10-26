3793918b5ae702de30c3445bb814bd6489273102
Poe <piotr@ptak.dev>
2019-10-26 09:16:43 +0200

    Implement progress bar

--- src/components/Tutorial/Tutorial.jsx
+++ src/components/Tutorial/Tutorial.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 export default class Tutorial extends Component {
   constructor() {
     super();
+
+    this.state = {
+      progress: 0,
+    };
+
+    this.increaseProgress = this.increaseProgress.bind(this);
+    this.loopProgress = this.loopProgress.bind(this);
+  }
+
+  componentDidMount() {
+    this.loopProgress();
+  }
+
+  increaseProgress(value) {
+    const { progress } = this.state;
+    const newProgress = progress + value;
+    if (newProgress <= 100) {
+      this.setState({ progress: newProgress });
+    }
+  }
+
+  loopProgress() {
+    const { progress } = this.state;
+    this.increaseProgress();
+    if (progress < 100) {
+      this.increaseProgress(2);
+      setTimeout(this.loopProgress, 100);
+    }
   }
 
   render() {
+    const { progress } = this.state;
     return (
       <main>
-        asf
+        <i className="nes-octocat animate" />
+        <ProgressBar value={progress} />
       </main>
     );
   }
