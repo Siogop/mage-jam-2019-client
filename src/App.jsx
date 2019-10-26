e8940e9a20f7611e5b9c6841d4b09e241dc4e7b6
Poe <piotr@ptak.dev>
2019-10-26 04:01:37 +0200

    add phases prototype to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
 import Main from './components/Main/Main';
 import './styles/global.scss';
 import '../node_modules/nes.css/css/nes.min.css';
 import AppHeader from './components/AppHeader/AppHeader';
 import AppFooter from './components/AppFooter/AppFooter';
 
-function App() {
-  return (
-    <div className="layout">
-      <div className="layout__screen">
-        <AppHeader />
-        <Main />
-        <AppFooter />
-      </div>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      phase: 0,
+    };
+
+    this.renderController = this.renderController.bind(this);
+    this.renderTutorial = this.renderTutorial.bind(this);
+  }
+
+  renderTutorial() {
+    return (
+      <div>
+        tutorial
+      </div>
+    );
+  }
+
+  renderController() {
+    return (
+      <div className="layout">
+        <div className="layout__screen">
+          <AppHeader />
+          <Main />
+          <AppFooter />
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { phase } = this.state;
+
+    switch (phase) {
+    case 0: return this.renderTutorial();
+    case 1: return this.renderController();
+    default: return <div />;
+    }
+  }
+}
