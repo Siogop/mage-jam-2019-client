5d4adf2bab09092311302aac838067b68e23667b
pankroscog <57038088+pankroscog@users.noreply.github.com>
2019-11-29 10:33:08 +0100

    ws server

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ import Connector from '../Connector/Connector';
 import Character from '../Character/Character';
 import Tutorial from '../Tutorial/Tutorial';
 
-const WS_ADDRESS = 'wss://b40347e4.ngrok.io/game';
+const WS_ADDRESS = 'wss://e13936e0.ngrok.io/game';
 
 class Main extends React.Component {
   // instance of websocket connection as a class property
